77febd17459041a4f5fce5660b42b2a55a8efa4e
Cesar1227 <bonicesar94@gmail.com>
2023-07-06 10:02:54 -0500

    WebConfig

---
+++ src/main/java/com/unillanos/listasuper/infraestructure/config/RestApiServletConfig.java
@@ -0,0 +1,35 @@
+package com.unillanos.listasuper.infraestructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class RestApiServletConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "http://localhost:8080")
+                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
+}
+
+
